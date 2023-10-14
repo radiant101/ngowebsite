341b9a2ed26dd0bafade5754d59a207e7a6314ee
radiant101 <aanuragsawant@gmail.com>
2023-10-14 11:45:58 +0530

    connected to navbar

--- ngoweb/home/views.py
+++ ngoweb/home/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+# Create your views here from here you display the http respons tto the site.
 def home (request):
     #return HttpResponse("THIS IS MY HOMEPAGE(/)")
     return render(request, 'home.html')
 
 def about (request):
-    return HttpResponse("THIS IS MY  about PAGE(/about)")
-
+    #return HttpResponse("THIS IS MY  about PAGE(/about)")
+     return render(request, 'about.html')
 def ngos (request):
-    return HttpResponse("THIS IS MY ngos PAGE(/)")
-
+   # return HttpResponse("THIS IS MY ngos PAGE(/)")
+    return render(request, 'ngos.html')
 def contact (request):
-    return HttpResponse("THIS IS MY contact PAGE(/)")
+   # return HttpResponse("THIS IS MY contact PAGE(/)")
+    return render(request, 'contact.html')
